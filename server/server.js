cbcce9cb9db2d26018087e0d3eaca6392a04930c
Andrew Johnson <andrew.johnson612@gmail.com>
2017-07-20 22:42:09 -0400

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 /* jshint esversion: 6 */
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -49,14 +50,12 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.delete('/todos/:id', (req, res) => {
-	// get the id
 	var id = req.params.id;
-	// validate the id -> not valid? return 404
+
 	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
-	// remove todo by id
 	Todo.findByIdAndRemove(id).then((todo) => {
 		if (!todo) {
 			return res.status(404).send();
@@ -64,11 +63,32 @@ app.delete('/todos/:id', (req, res) => {
 
 		res.send({todo});
 	}).catch((e) => res.status(400).send());
-		// success
-			// if no doc, send 404
-			// if doc, send 200 with doc
-		// error
-			// 400 with empty body
+});
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	}	else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
 });
 
 app.listen(port, () => {
